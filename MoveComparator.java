f74a1d0471a1ea056323e15358bf52de86793aa9
Hendrikto <hendrik.to@gmail.com>
2016-02-12 00:49:07 +0100

    Implement the Comparator interface for Move objects.

---
+++ MoveComparator.java
@@ -0,0 +1,38 @@
+package assignment2;
+
+import java.util.Comparator;
+
+/**
+ * Implements the Comparator interface for moves to be able to sort them.
+ *
+ * @author Hendrik Werner // s4549775
+ * @author Jasper Haasdijk // s4449754
+ */
+public class MoveComparator implements Comparator<Move> {
+    private final Position position;
+    private final Position centerPosition;
+    
+    /**
+     * Constructor method.
+     * 
+     * @param position Current knight position.
+     * @param center Center position of the board.
+     */
+    public MoveComparator (Position position, Position center) {
+        this.position = position;
+        this.centerPosition = center;
+    }
+    
+    /**
+     * Compare two moves.
+     * 
+     * @param a First move to compare.
+     * @param b Second move to compare.
+     * @return The difference between the distance to the center after applying a and b respectively to position. (a - b)
+     */
+    @Override
+    public int compare (Move a, Move b) {
+        return ((position.apply(a).distanceTo(centerPosition))
+                - (position.apply(b).distanceTo(centerPosition)));
+    }
+}
